c66a55377d7150bc47c638ea01170785c543272a
yeshuaaguilar <76400533@certus.edu.pe>
2021-09-29 10:59:01 -0500

    v1.5

---
+++ js/clock.js
@@ -0,0 +1,33 @@
+const getRemainingTime = deadline => {
+    let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+        remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        remainDays = Math.floor(remainTime / (3600 * 24));
+  
+    return {
+      remainSeconds,
+      remainMinutes,
+      remainHours,
+      remainDays,
+      remainTime
+    }
+  };
+  
+  const countdown = (deadline,elem,finalMessage) => {
+    const el = document.getElementById(elem);
+  
+    const timerUpdate = setInterval( () => {
+      let t = getRemainingTime(deadline);
+      el.innerHTML = `${t.remainDays}d:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s`;
+  
+      if(t.remainTime <= 1) {
+        clearInterval(timerUpdate);
+        el.innerHTML = finalMessage;
+      }
+  
+    }, 1000)
+  };
+  
+  countdown('October 20 2021 21:34:40 GMT-0500', 'clock', '¡Ya empezó!');
